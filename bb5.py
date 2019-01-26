9a9cccd81918e41d0a5a95693aa4d205ce609b41
niranj1234 <46585465+niranj1234@users.noreply.github.com>
2019-01-26 22:09:20 +0530

    Update bb5.py

--- bb5.py
+++ bb5.py
@@ -1,7 +1,7 @@
-x,z,y=map(int,input().split())
+x,y,z=map(int,input().split())
 if x>z and x>y:
     print("x is greater")
-elif z>y:
-    print("z is greater")
-else:
+elif y>z:
     print("y is greater")
+else:
+    print("z is greater")
