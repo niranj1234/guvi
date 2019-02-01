8606584bedd6d3445fb02672a2f2866d23d03ad6
niranj1234 <46585465+niranj1234@users.noreply.github.com>
2019-02-01 08:44:47 +0530

    Update bb15.py

--- bb15.py
+++ bb15.py
@@ -1,7 +1,12 @@
+
 A,B=map(int,input().split())
+
 for i in range(A+1,B):
-    if(i%2==0):
+
+    if(i%2==0 and i!=B-1):
+
         print(i,end=" ")
+
     elif(i%2==0 and i==B-1):
+
         print(i)
-        
