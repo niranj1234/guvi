9b04da076f547d092fc67bec67cb52baf05f8afc
niranj1234 <46585465+niranj1234@users.noreply.github.com>
2019-02-05 18:26:19 +0530

    Update bb16.py

--- bb16.py
+++ bb16.py
@@ -1,9 +1,9 @@
-a,b=map(int,input().split())
-for num in range(a+1 ,b):
-    if (num > 1): 
-        for i in range (2,num):
-            if ((num % i) == 0):
-                break
-            else:
-                print(num,end=" ")
-                break
+p,r=map(int,input().split())
+for a in range(p+1,r):
+    k=0
+    for i in range(2,a//2+1):
+        if(a%i==0):
+            k=k+1
+    if(k<=0):
+        print(a,end=' ')
+        
