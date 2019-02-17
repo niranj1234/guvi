eca0664ff4e4cdc1d39d158ad3e1a61516619693
niranj1234 <46585465+niranj1234@users.noreply.github.com>
2019-02-17 18:47:47 +0530

    Update bb29.py

--- bb29.py
+++ bb29.py
@@ -1,9 +1,12 @@
 a=int(input())
 c=a%60
 d=a/60
-e=round(d)-1
-if(e<1):
-    f=0 
-    print(f,c)
+e=str(d)
+f=e[:e.index('.')]
+f=int(f)
+if(f<1):
+    h=0 
+    print(h,c)
 else:
-    print(e,c)
+    print(f,c)
+    
